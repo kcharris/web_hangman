81ddf2ff9a293f19cf07c7c6b224a165b30fab47
kevin <kcharris77@gmail.com>
2019-05-26 11:10:32 -0500

    add a lot, still needs game over text

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Current issues:
 1 Translate puts messages into something front end can use.
 2 I don't know if the guess that shows up at the end of address is a route or how I can be expected to use multiple routes to solve a problem unless it expects me to store the data somehow like in a loop or params[] added variable, that might alter route.
 3 Move letter selection to front end.
+4 store player input in either a params variable or in session info
--- web_hangman.rb
+++ web_hangman.rb
@@ -1,14 +1,45 @@
 require "sinatra"
 require "sinatra/reloader"
-require "./lib/hangman.1.rb"
+require "./lib/hangman.rb"
 
 include Hangman
+
+enable :sessions
+
 game = Game.new
-player = Player.new
+game.new_game
+
+post "/" do
+  game.guesses_left = session[:guesses_left]
+  game.guessed_letters = session[:guessed_letters]
+  game.answer_board = session[:answer_board]
+  game.word_to_guess = session[:word_to_guess]
+  game.game_over = session[:game_over]
+  game.win = session[:win]
+end
 
 get "/" do
-  prompt = game.game_prompt
-  erb :index, :locals => {:prompt => prompt, :word_to_guess => word_to_guess,
-                          :current_guess => current_guess, :answer_board => answer_board,
-                          :guesses_left => guesses_left, :output => output}
+  if params["guess"] != nil
+    game.input_processor(params["guess"])
+  end
+  unless params["guess"] =~ /^[a-zA-Z]$/
+    @error = true
+  else
+    @error = false
+  end
+  if params["cheat"]
+    cheat = true
+  else
+    cheat = false
+  end
+  session[:guesses_left] = game.guesses_left
+  session[:guessed_letters] = game.guessed_letters
+  session[:answer_board] = game.answer_board
+  session[:word_to_guess] = game.word_to_guess
+  session[:game_over] = game.game_over
+  session[:win] = game.win
+  erb :index, :locals => {:guesses_left => game.guesses_left,
+                          :error => @error, :guessed_letters => game.guessed_letters,
+                          :answer_board => game.answer_board, :word_to_guess => game.word_to_guess,
+                          :game_over => game.game_over, :win => game.win, :cheat => cheat}
 end
